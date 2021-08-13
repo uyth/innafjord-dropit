fd053505211440056790cb25b45e86472f04160c
Uy Huynh <uyth95@gmail.com>
2021-08-13 10:17:28 +0200

    Align modules

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-import { Turbines, Chart, WaterBox, WaterPaper, ProductionPaper} from './components';
+import { Turbines } from './components';
 import Box1 from './components/Box1';
 import Box2 from './components/Box2';
 import { Grid } from '@material-ui/core';
 
 
-const data = [{name: '10.08', uv: 400, pv: 2400, amt: 10000}, 
-            {name: '11.08', uv: 500, pv: 2400, amt: 2400} , {name: '12.08', uv: 600, pv: 2400, amt: 2400}];
-
 function App() {
   return (
     <div className="App">
-      <Grid container>
-        <Grid item xs="6"><Box1/></Grid>
-        <Grid item xs="6"><Box2/></Grid>
+      <Grid container xs="12">
+        <Grid item xs="6">
+          <Box1/>
+        </Grid>
+        <Grid item xs="6">
+          <Box2/>
+        </Grid>
       </Grid>
-      <Turbines/>
+      <Grid item xs="12">
+        <Turbines/>
+      </Grid>
     </div>
   );
 }
