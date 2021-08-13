e1465cbf757742b31193ce4b28121380f06f70b6
Uy Huynh <uyth95@gmail.com>
2021-08-13 11:21:43 +0200

    Add chart for power generated

--- src/components/Box2.js
+++ src/components/Box2.js
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Turbine, Chart, WaterBox, ProductionPaper} from '.';
-import {PowerPrice, Money} from './Charts';
+import { Turbine, Chart, WaterBox, ProductionPaper } from '.';
+import { PowerPrice, Money, PowerLevel } from './Charts';
 import { Box } from '@material-ui/core';
-
-//const data = [{name: '10.08', uv: 400, pv: 2400, amt: 10000}, 
-  //          {name: '11.08', uv: 500, pv: 2400, amt: 2400} , {name: '12.08', uv: 600, pv: 2400, amt: 2400}];
 
 
 function Box2() {
   return (
       <ProductionPaper title="Strøm Produsert">
-        <Box>
-
+        <Box display="flex" flexDirection="row" justifyContent="center">
+          <PowerLevel/>
         </Box>
         <Box display="flex" justifyContent="space-evenly">
           <Money/>
--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -1,7 +1,8 @@
 import WaterLevel from "./Waterlevel";
+import PowerLevel from "./PowerLevel";
 import WaterInflux from "./WaterInflux";
 import EnvCost from './EnvCost';
 import PowerPrice from "./PowerPrice";
 import Money from "./Money";
 
-export {WaterLevel, WaterInflux, PowerPrice, Money,EnvCost};
+export {WaterLevel, WaterInflux, PowerPrice, Money,EnvCost, PowerLevel};
